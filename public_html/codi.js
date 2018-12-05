07d10c21e12576a2e55993c4bcdee935ccc17c67
LAPTOP-NTCGK8E9\alexa <alexalba123@gmail.com>
2018-12-05 19:36:27 +0100

    Modificació funció iniJoc. Codificació funció mostraEspai, on mostra per pantalla tot els que interessa. Codificació funció del càlcul de la següent peça en caure. Creació funcions gestió interacció amb teclat i moviment automàtic del joc. Test retorn funció següent peça. Comentaris varis.

--- public_html/codi.js
+++ public_html/codi.js
@@ -1,29 +1,58 @@
 var Joc = {
-    estat : 0,
-    puntJugador : 0,
-    puntMaxJugador : 0,
-    peçaVigent : [],
-    peçaSeguent : [],
-    comptadorPeces : 0,
-    interval : 1000,
-    espai : new Array(),
-    iniEspai : function(){
+    estat: 0,
+    puntJugador: 0,
+    puntMaxJugador: 0,
+    peçaVigent: [],
+    peçaSeguent: [],
+    comptadorPeces: [i = 0, j = 0, l = 0, o = 0, s = 0, t = 0, z = 0],
+    interval: 1000,
+    nivell: 0,
+    espai: new Array(),
+    //Funció que inicialitza el joc
+    iniJoc: function () {
         this.puntJugador = 0;
+        this.nivell = 1;
         for (var i = 0; i < 25; i++) {
-            this.espai.push([0,0,0,0,0,0,0,0,0,0]);
+            this.espai.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
         }
-        
     },
-    calcPesaSeg : function(){
+    //Funció encarregada de mostrar l'espai(mapa, puntuacions i nivell)
+    mostraEspai: function () {
+        for (var i = 0; i < this.espai.length; i++) {
+            document.write(this.espai[i] + "<br>");
+        }
+        document.write("<br>Puntuació: " + this.puntJugador);
+        document.write("<br>Puntuació Màxima: " + this.puntMaxJugador);
+        document.write("<br>Nivell: <br>");
+    },
+    //Funció encarregada de calcular quina serà la següent peça en caure
+    calcPesaSeg: function () {
         var peces = [
-            [[[0,0,0,0,0,0],[0,0,1,1,0,0],[0,0,1,1,0,0],[0,0,0,0,0,0]],"groc"],
-            [[[0,1,0,0],[0,1,0,0],[0,0,1,1,0,0],[0,0,1,1,0,0]],"cel"]
-        ]
+            [[[1, 1], [1, 1]], "groc"],
+            [[[0, 0, 0], [1, 0, 0], [1, 1, 1]], "blau"],
+            [[[0, 0, 0], [0, 0, 1], [1, 1, 1]], "taronja"],
+            [[[0, 0, 0], [0, 1, 1], [1, 1, 0]], "verd"],
+            [[[0, 0, 0], [0, 1, 0], [1, 1, 1]], "lila"],
+            [[[0, 0, 0], [1, 1, 0], [0, 1, 1]], "vermell"],
+            [[[0, 1, 0, 0], [0, 1, 0, 0], [0, 1, 0, 0], [0, 1, 0, 0]], "cel"]
+        ];
+        var rand = Math.floor(Math.random() * (7 - 0) + 0);
+        return peces[rand];
     },
-    teclaClic : function(){
-        
+    //Funció encarregada de la gestió de la interacció de teclat de l'usuari
+    teclaClic: function () {
+        //onKeyDown
     },
-    movPesa : function(){
-        
+    //Funció que realitza el moviment automàtic del joc cada cert interval de temps
+    movPesa: function () {
+        //setInterval
     }
 };
+
+Joc.iniJoc();
+Joc.mostraEspai();
+
+document.write("<br>Test Seguent Pesa: <br>")
+for (var i = 0; i < 5; i++) {
+    document.write(Joc.calcPesaSeg() + "<br>");
+}
